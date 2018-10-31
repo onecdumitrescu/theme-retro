e0bd9e990f1b4b35273a6247703e3ea7ce195390
Catalin Dumitrescu <catalin.dumitrescu@expremio.com>
2018-10-31 11:28:00 +0200

    updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,7 +11,15 @@ done
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar top &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload top &
+    done
+else
+    polybar --reload top &
+fi
+
+#polybar top &
 #polybar top-second &
 
 echo "Bars launched..."
